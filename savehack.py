64f892b5fdfe8f7e1f89352c77cce5800d7ccb79
codehacker554 <104781383+codehacker554@users.noreply.github.com>
2022-05-02 11:41:04 -0400

    Update savehack.py

--- savehack.py
+++ savehack.py
@@ -3,19 +3,19 @@ import base64
 print("Idle Breakout Save Hack by Xsus")
 
 print("What level do you want to be on?")
-level = input()
+level = input(10000)
 
 print("What amount of money would you like to have")
-money = input()
+money = input(200000000)
 
 print("How much gold do you want")
-gold = input()
+gold = input(100000000)
 
 print("How many Black Boxes?")
-bb = input()
+bb = input(99999)
 
 print("How many skillpoints")
-sp = input()
+sp = input(1000)
 
 s = f"{level},{money},{gold},2,0,0,0,0,0,0,0,1,1,0,43595.78,999999,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{bb},0,0,0,1,{sp},1,0,0"
 b = s.encode("UTF-8")
